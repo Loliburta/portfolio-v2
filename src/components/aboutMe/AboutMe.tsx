a015ba6507fc5b24496bb9a44270c68d3ed77b4f
Loliburta <mateusz.maranowski@protonmail.com>
2021-05-03 19:44:02 +0200

    mobile friendly for aboutme and hero

--- src/components/aboutMe/AboutMe.tsx
+++ src/components/aboutMe/AboutMe.tsx
@@ -3,13 +3,16 @@ export const AboutMe = () => {
     <div className="aboutMe">
       <div className="aboutMe__heading"> About Me</div>
       <div className="aboutMe__description">
-        Welcome and thank you for checking out my site! My name is Mateusz and I
-        enjoy creating new things on the web and tackling difficult problems. My
-        jurney with programming started in 2019 when I watched my brother
-        programming and thought it was so cool that I should try it. So I tried
-        and put my first steps in python alghoritms and basic scripting and then
-        expanded to building websites using html css js which progressed to
-        typescript with react and became my main point of interest
+        Welcome and thank you for checking out my site! My name is Mateusz, I
+        enjoy creating new things on the web and tackling difficult problems.
+        <p>
+          My jurney with programming started in 2019 when I watched my brother
+          programming and thought it was so cool that I should try it. So I
+          tried and put my first steps in python alghoritms and basic scripting
+          and then expanded to building websites using html css js which
+          progressed to typescript with react and became my main point of
+          interest
+        </p>
         <p>Here are few technologies I've been working with recently:</p>
       </div>
       <ul className="aboutMe__skillsUl">
