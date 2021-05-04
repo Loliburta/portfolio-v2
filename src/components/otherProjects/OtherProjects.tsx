62f3a2a57b3d42af8ccab609bc56e539708510d1
Loliburta <mateusz.maranowski@protonmail.com>
2021-05-04 13:23:32 +0200

    project resizing update

--- src/components/otherProjects/OtherProjects.tsx
+++ src/components/otherProjects/OtherProjects.tsx
@@ -4,7 +4,11 @@ export const OtherProjects = () => {
   return (
     <div className="otherProjects">
       <div className="otherProjects__heading">Other Cool Projects</div>
-      <div className="otherProjects__github"><a href="https://github.com/Loliburta" target="_blank">view my github</a></div>
+      <div className="otherProjects__github">
+        <a href="https://github.com/Loliburta" target="_blank" rel="noreferrer">
+          view my github
+        </a>
+      </div>
       <div className="otherProjects__projects">
         <div className="otherProjects__projects__item">1</div>
         <div className="otherProjects__projects__item">2</div>
