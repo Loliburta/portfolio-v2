74855709fa8239d38480e1cf6d4757638eb6a3b6
Loliburta <mateusz.maranowski@protonmail.com>
2021-05-04 19:23:05 +0200

    flipped project

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -4,15 +4,14 @@ export const Contact = () => {
   return (
     <div className="contact--wrapper">
       <img src={Buildings} alt="buildings" className="contact--wrapper__buildings" />
-
-      <div className="contact">
-        <div className="contact__heading">Contact</div>
-        <div className="contact__description">
+      <div className="contact--wrapper__contact">
+        <div className="contact--wrapper__contact__heading">Contact</div>
+        <div className="contact--wrapper__contact__description">
           If you wanna get in touch, talk to me about a project collaboration or
           just say hi, click on this awesome button below or send an email to
           Maranowski.Mateusz@gmail.com
         </div>
-        <div className="contact__button">Send Mail</div>
+        <div className="contact--wrapper__contact__button">Send Mail</div>
       </div>
     </div>
   );
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -8,6 +8,94 @@ export const Projects = () => {
     <div className="projects">
       <div className="projects__heading">Some Things I've Built</div>
       <ul className="projects__list">
+        {/* First project */}
+
+        <li className="projects__list__item">
+          <div className="projects__list__item__content">
+            <div className="projects__list__item__content__overline">
+              Featured Project
+            </div>
+            <div className="projects__list__item__content__title">
+              Crypto Price Tracker
+            </div>
+            <div className="projects__list__item__content__description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam
+            </div>
+            <div className="projects__list__item__content__tech">
+              <div className="projects__list__item__content__tech__item">
+                React
+              </div>
+              <div className="projects__list__item__content__tech__item">
+                Typescript
+              </div>
+              <div className="projects__list__item__content__tech__item">
+                SCSS
+              </div>
+            </div>
+            <div className="projects__list__item__content__links">
+              <div className="projects__list__item__content__links__icon">
+                <Icon icon={githubFill} height="20px" />
+              </div>
+              <div className="projects__list__item__content__links__icon">
+                <Icon icon={externalIcon} height="20px" />
+              </div>
+            </div>
+          </div>
+          <div className="projects__list__item__imageDiv">
+            <img
+              src={img1}
+              alt="project 1"
+              className="projects__list__item__imageDiv__img"
+            />
+          </div>
+        </li>
+        {/* Second project */}
+
+        <li className="projects__list__item">
+          <div className="projects__list__item__content content--flipped">
+            <div className="projects__list__item__content__overline">
+              Featured Project
+            </div>
+            <div className="projects__list__item__content__title">
+              Crypto Price Tracker
+            </div>
+            <div className="projects__list__item__content__description description--flipped">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam
+            </div>
+            <div className="projects__list__item__content__tech">
+              <div className="projects__list__item__content__tech__item">
+                React
+              </div>
+              <div className="projects__list__item__content__tech__item">
+                Typescript
+              </div>
+              <div className="projects__list__item__content__tech__item">
+                SCSS
+              </div>
+            </div>
+            <div className="projects__list__item__content__links">
+              <div className="projects__list__item__content__links__icon">
+                <Icon icon={githubFill} height="20px" />
+              </div>
+              <div className="projects__list__item__content__links__icon">
+                <Icon icon={externalIcon} height="20px" />
+              </div>
+            </div>
+          </div>
+          <div className="projects__list__item__imageDiv imageDiv--flipped">
+            <img
+              src={img1}
+              alt="project 1"
+              className="projects__list__item__imageDiv__img"
+            />
+          </div>
+        </li>
+        {/* Third Project */}
+
         <li className="projects__list__item">
           <div className="projects__list__item__content">
             <div className="projects__list__item__content__overline">
