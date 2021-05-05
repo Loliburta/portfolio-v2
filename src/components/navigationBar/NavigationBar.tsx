79dea2303c81b990c34d177dd4755898c11c2b92
Loliburta <mateusz.maranowski@protonmail.com>
2021-05-05 16:50:23 +0200

    menu animations

--- src/components/navigationBar/NavigationBar.tsx
+++ src/components/navigationBar/NavigationBar.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import Logo from "../../icons/logo.svg";
-import { Icon } from "@iconify/react";
-import menu from "@iconify-icons/bx/bx-menu-alt-right";
+import { Squash as Hamburger } from "hamburger-react";
 export const NavigationBar = () => {
   const [active, isActive] = useState(false);
+  const [isOpen, setOpen] = useState(false);
   return (
     <div className="navBar">
       <img src={Logo} alt="logo" className="navBar__logo" />
-      <div className="menu" onClick={() => isActive(!active)}>
-        <Icon icon={menu} />
+      <div className="navBar__hamburger " onClick={() => isActive(!active)}>
+        <Hamburger toggled={isOpen} toggle={setOpen} />
       </div>
       <div className="navBar__links">
         <div className="navBar__links__link">01. About</div>
@@ -16,8 +16,8 @@ export const NavigationBar = () => {
         <div className="navBar__links__link">03. Contact</div>
         <div className="navBar__links__resume">Resume</div>
       </div>
-      <div className={active ? "navBar__menu" : "navBar__menu--active"}>
-        <div className="navBar__menu__link">01. About</div>
+      <div className={active ? "navBar__menu--active" : "navBar__menu"}>
+        <div className="navBar__menu__slink">01. About</div>
         <div className="navBar__menu__link">02. Projects</div>
         <div className="navBar__menu__link">03. Contact</div>
         <div className="navBar__menu__resume">Resume</div>
