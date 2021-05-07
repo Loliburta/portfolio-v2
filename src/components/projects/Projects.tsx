7f2366cc56d5532e066eb986ff956d0830e26aa6
Loliburta <mateusz.maranowski@protonmail.com>
2021-05-07 21:47:49 +0200

    small screens friendly

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,11 +1,15 @@
 import { Project1 } from "./project/Project1";
 import { Project2 } from "./project/Project2";
 import { Project3 } from "./project/Project3";
+import { useWindowWidth } from "@react-hook/window-size";
 
 export const Projects = () => {
+  const screenWidth = useWindowWidth();
   return (
     <div className="projects">
-      <div className="projects__heading">Some Things I've Built</div>
+      <div className="projects__heading">
+        {screenWidth > 350 ? "Some Things I've Built" : "Things I've Built"}
+      </div>
       <ul className="projects__list">
         <Project1 />
         <Project2 />
